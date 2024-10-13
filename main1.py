11b51ab93b24f2ec945eb1f5eb06594eed6ba923
Yannys-coder <ccyannys123@gmail.com>
2024-10-13 11:34:38 -0400

    first commit

---
+++ main1.py
@@ -0,0 +1,11 @@
+import os
+# print("Does new file exist?")
+# if os.path.exists("New_file.txt"):
+#     os.remove("New_file.txt")
+# else:
+#     print("File does not exist")
+
+
+# os.remove('New_file.txt')
+
+os.rmdir('Delete')
